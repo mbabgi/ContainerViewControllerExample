cda891af6695953e51f56f2595a02d34cf59db77
Sam Watts <sam@futureworkshops.com>
2013-03-25 16:10:06 +0300

    Initial Commit

---
+++ SlideOutMenuContainerViewControllerExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideOutMenuContainerViewControllerExample
+//
+//  Created by Sam Watts on 25/03/2013.
+//  Copyright (c) 2013 Sam Watts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
